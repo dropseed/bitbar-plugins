41c26c12ae26a8f2227c95f0c62f63babfaf501e
Dave Gaeddert <dave.gaeddert@gmail.com>
2020-01-08 16:04:33 -0600

    Update time.30s.sh

--- time.30s.sh
+++ time.30s.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 date -u +'%H:%M UTC'
 echo '---'
+echo -n "UTC " ; TZ=":UTC" date +'%l:%M %p'
 echo -n "NY (Eastern) " ; TZ=":US/Eastern" date +'%l:%M %p'
 echo -n "KS (Central) " ; TZ=":US/Central" date +'%l:%M %p'
 echo -n "SF (Pacific) " ; TZ=":US/Pacific" date +'%l:%M %p'
